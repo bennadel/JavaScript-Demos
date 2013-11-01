eb45aec99089dc96b7f3876567baaee0f227aab8
Ben Nadel <ben@bennadel.com>
2013-11-01 08:36:52 -0400

    Add the requirejs service demo.

---
+++ demos/requirejs-service-angularjs/friends.js
@@ -0,0 +1,26 @@
+// Define the friends data using the RequireJS global utility.
+define(
+	[ /* No dependencies. */ ],
+	[
+		{
+			id: 1,
+			name: "Sarah",
+			catchPhrase: "Awesome sauce!"
+		},
+		{
+			id: 2,
+			name: "Tricia",
+			catchPhrase: "Blam!"
+		},
+		{
+			id: 3,
+			name: "Joanna",
+			catchPhrase: "That's what I'm talking about!"
+		},
+		{
+			id: 4,
+			name: "Heather",
+			catchPhrase: "Yeah, boy!"
+		}
+	]
+);
