169d4666db6301edc96b1c093e618e4c9c42c1ae
Ben Nadel <ben@bennadel.com>
2018-05-06 07:57:11 -0400

    Added hello-world for Angular 6 libraries.

---
+++ demos/hello-world-angular6/app/app.component.ts
@@ -0,0 +1,23 @@
+
+// Import the core angular services.
+import { Component } from "@angular/core";
+
+// ----------------------------------------------------------------------------------- //
+// ----------------------------------------------------------------------------------- //
+
+@Component({
+	selector: "my-app",
+	styleUrls: [ "./app.component.less" ],
+	template:
+	`
+		Hello world, Angular 6 compiled.
+	`
+})
+export class AppComponent {
+
+	// I initialize the app-component.
+	constructor() {
+		// ...
+	}
+
+}
